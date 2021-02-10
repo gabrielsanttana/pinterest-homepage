766cc9692c4e9cbcadd2e7547e7ab3ff83c1e93e
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-02-09 22:51:20 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Pinterest</h1>
+    </div>
+  );
+};
+
+export default App;
