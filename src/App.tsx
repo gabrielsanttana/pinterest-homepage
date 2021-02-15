2f5bbcc0738625bb9b031cd1ae316d6d0830c770
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-02-15 15:12:31 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -29,26 +29,34 @@ const App: React.FC = () => {
 
         <div className={styles.phrasesWrapper}>
           <div className={styles.offset}>
-            <h2 className={styles.phrase}>summer outfit ideais</h2>
+            <h2 className={`${styles.phrase} ${styles.animationBefore}`}>
+              summer outfit ideais
+            </h2>
           </div>
 
           <div className={styles.offset}>
-            <h2 className={styles.phrase}>activity for kids</h2>
+            <h2 className={`${styles.phrase} ${styles.animationBefore}`}>
+              activity for kids
+            </h2>
           </div>
 
           <div className={styles.offset}>
-            <h2 className={styles.phrase}>DYI project</h2>
+            <h2 className={`${styles.phrase} ${styles.animationBefore}`}>
+              DYI project
+            </h2>
           </div>
 
           <div className={styles.offset}>
-            <h2 className={styles.phrase}>idea for special dinner</h2>
+            <h2 className={`${styles.phrase} ${styles.animationBefore}`}>
+              idea for special dinner
+            </h2>
           </div>
         </div>
       </div>
 
       <div className={styles.gridContainer}>
         <div className={styles.grid}>
-          <div className={styles.column}>
+          <div className={`${styles.column} ${styles.animationBefore}`}>
             <div className={styles.item}></div>
 
             <div className={styles.item}></div>
@@ -60,7 +68,7 @@ const App: React.FC = () => {
             <div className={styles.item}></div>
           </div>
 
-          <div className={styles.column}>
+          <div className={`${styles.column} ${styles.animationBefore}`}>
             <div className={styles.item}></div>
 
             <div className={styles.item}></div>
@@ -72,7 +80,7 @@ const App: React.FC = () => {
             <div className={styles.item}></div>
           </div>
 
-          <div className={styles.column}>
+          <div className={`${styles.column} ${styles.animationBefore}`}>
             <div className={styles.item}></div>
 
             <div className={styles.item}></div>
@@ -84,7 +92,7 @@ const App: React.FC = () => {
             <div className={styles.item}></div>
           </div>
 
-          <div className={styles.column}>
+          <div className={`${styles.column} ${styles.animationBefore}`}>
             <div className={styles.item}></div>
 
             <div className={styles.item}></div>
@@ -96,7 +104,7 @@ const App: React.FC = () => {
             <div className={styles.item}></div>
           </div>
 
-          <div className={styles.column}>
+          <div className={`${styles.column} ${styles.animationBefore}`}>
             <div className={styles.item}></div>
 
             <div className={styles.item}></div>
@@ -108,7 +116,7 @@ const App: React.FC = () => {
             <div className={styles.item}></div>
           </div>
 
-          <div className={styles.column}>
+          <div className={`${styles.column} ${styles.animationBefore}`}>
             <div className={styles.item}></div>
 
             <div className={styles.item}></div>
@@ -120,7 +128,7 @@ const App: React.FC = () => {
             <div className={styles.item}></div>
           </div>
 
-          <div className={styles.column}>
+          <div className={`${styles.column} ${styles.animationBefore}`}>
             <div className={styles.item}></div>
 
             <div className={styles.item}></div>
---
+++ src/script.js
@@ -0,0 +1,6 @@
+// import styles from './App.module.scss';
+
+// const grid = document.querySelector(styles.grid);
+// const headingPhrases = document.querySelectorAll(
+//   `${styles.heading} ${styles.phrasesWrapper} ${styles.phrase}`,
+// );
